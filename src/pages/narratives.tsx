573687c37a5a5e72930f6a43be9e48c9156d1f93
Serge <sergesoroka@gmail.com>
2023-04-03 08:57:40 +0300

    BarChart Narratives: interactive

--- src/pages/narratives.tsx
+++ src/pages/narratives.tsx
@@ -4,37 +4,51 @@ import { uniqueNarrativesEn } from "../../utils/statisticCalculate";
 import data from "../../data/dataEn.json";
 import Link from "next/link";
 import { useState } from "react";
+import LatestNarratives from "@/components/LatestNarratives/LatestNarratives";
+
+let defaultNarrative = 'Narrative concerning weapons provided to Ukraine by Western countries'
 
 const Narratives = () => {
-  const [title, setTitle] = useState<string | null>(null);
+  const [title, setTitle] = useState<string | null>(defaultNarrative);
+  const [fakes, setFakes] = useState<number>(0);
 
-  const uniqueFakes: string[] = [];
   const renderNarratives = uniqueNarrativesEn.map((item, i) => {
+    const uniqueFakes: string[] = [];
+
     data.map((fake) => {
       if (!uniqueFakes.includes(fake.Fake) && fake.Narrative === item) {
         uniqueFakes.push(fake.Fake);
       }
     });
     return (
-      <rect
-        key={i}
-        width="40"
-        height={uniqueFakes.length * 3}
-        fill={title === item ? "#ff2618" : "#ccc"}
-        x={i * 50}
-        style={{ cursor: "pointer" }}
-        onClick={() => setTitle(item)}
-      />
+      <>
+        <rect
+          key={i}
+          width="20"
+          height={uniqueFakes.length * 20}
+          fill={title === item ? "#ff2618" : "#ccc"}
+          x={i * 30}
+          style={{ cursor: "pointer" }}
+          onClick={() => {setTitle(item)
+            // @ts-ignore
+         setFakes(uniqueFakes.length) 
+          }}
+        />
+      </>
     );
   });
   return (
     <div className={styles.allNarrativesWrap}>
-      <svg width="830" height="200" style={{ transform: "scaleY(-1)" }}>
-        {renderNarratives}
-      </svg>
-      <Link href={{ pathname: `/narrative/${title}` }}>
-        <h3 className={styles.narrativeDynamicTitle}>{title}</h3>
-      </Link>
+      <div>
+        {fakes !== 0 && <p className={styles.fakesNumber}>{fakes} FAKES</p>}
+        <svg width="1200" height="140" style={{ transform: "scaleY(-1)" }}>
+          {renderNarratives}
+        </svg>
+        <Link href={{ pathname: `/narrative/${title}` }}>
+          <h3 className={styles.narrativeDynamicTitle}>{title}</h3>
+        </Link>
+      </div>
+      <LatestNarratives />
     </div>
   );
 };

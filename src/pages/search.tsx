e4c5afc4080be49c4f9c8f9e6b7fa3bf14ce2e6e
Serge <sergesoroka@gmail.com>
2023-04-12 17:29:35 +0300

    Search Pages Animation

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,12 +1,17 @@
 import Search from "@/components/Search/Search";
 import styles from "../styles/Home.module.css";
+import { motion } from "framer-motion";
 
 const search = () => {
-
   return (
-    <div className={styles.mainLeft}>
-     <Search />
-    </div>
+    <motion.div
+      initial={{ opacity: 0, y: -20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.6, type: "tween" }}
+      className={styles.mainLeft}
+    >
+      <Search />
+    </motion.div>
   );
 };
 

925243783126434b5c99f4a2f5e226a76f976d26
Serge <sergesoroka@gmail.com>
2023-04-18 21:08:30 +0300

    Five Narratives Data Reloaded

--- src/components/LatestNarratives/LatestNarratives.tsx
+++ src/components/LatestNarratives/LatestNarratives.tsx
@@ -1,31 +1,61 @@
 import Link from "next/link";
 import { motion } from "framer-motion";
 import styles from "./LatestNarratives.module.css";
-// import data from '../../../data/dataEn.json'
-import useLangSwitcher from "../../../utils/i18n/useLangSwitcher";
+// import useLangSwitcher from "../../../utils/i18n/useLangSwitcher";
+
+import narrLast from '../../../data/lastedNarratives.json'
+import { useRouter } from "next/router";
 
 const LatestNarratives = () => {
-  const { data } = useLangSwitcher();
+  const router = useRouter();
+  const { locale } = router;
+
+  const data =
+    locale == "ua"
+      ? narrLast['ua']
+      : locale == "de"
+      ? narrLast['de']
+      : locale == "pl"
+      ? narrLast['pl']
+      : locale == "en"
+      ? narrLast['en']
+      : locale == "sk"
+      ? narrLast['sk']
+      : locale == "it"
+      ? narrLast['it']
+      : locale == "hu"
+      ? narrLast['hu']
+      : locale == "cs"
+      ? narrLast['cs']
+      : locale == "ru"
+      ? narrLast['ru']
+      : narrLast['ua'];
+
+
+
+  // const { data } = useLangSwitcher();
 
   // @ts-ignore
 
-  const uniqueNarrative: string[] = [];
+  // const uniqueNarrative: string[] = [];
+  // console.log(uniqueNarrative);
+  
   // @ts-ignore
   const lastNarratives = data.map((narrative, i) => {
-    if (
-      !uniqueNarrative.includes(narrative.Narrative) &&
-      uniqueNarrative.length < 5
-    ) {
-      uniqueNarrative.push(narrative.Narrative);
+    // if (
+    //   !uniqueNarrative.includes(narrative.Narrative) &&
+    //   uniqueNarrative.length < 5
+    // ) {
+    //   uniqueNarrative.push(narrative.Narrative);
       return (
-        <Link key={i} href={{ pathname: `/narrative/${narrative.Narrative}` }}>
+        <Link key={i} href={{ pathname: `/narrative/${narrative}` }}>
           <motion.h1
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 0.5 }}
             className={styles.narrativeHeading}
           >
-            {narrative.Narrative}
+            {narrative}
           </motion.h1>
           <hr
             style={{
@@ -36,7 +66,7 @@ const LatestNarratives = () => {
           />
         </Link>
       );
-    }
+   
   });
 
   return (

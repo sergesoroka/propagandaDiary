712edbdd317892b5ce014223ab40c91f7e8db147
Serge <sergesoroka@gmail.com>
2023-04-18 11:23:20 +0300

    Localitation About: Fixed

--- src/components/About/AboutIt.tsx
+++ src/components/About/AboutIt.tsx
@@ -3,29 +3,7 @@ import React from "react";
 function AboutIt() {
   return (
     <>
-    <p class="c3">
-      <span class="c1">
-        VoxCheck è il progetto di fact-checking. Dal 2018 VoxCheck è un
-        firmatario dell Codice Etico del Network Internazionale dei
-        Fact-checkers presso Poynter Institute (IFCN) – la più grande comunità
-        professionale dei fact-checkers al mondo. Ed è anche un membro del
-        programma di fact-checking Meta, in cui segniamo e confutiamo il
-        contenuto falso condiviso dagli utenti della piattaforma. Puoi trovare
-        l’informazione ulteriore sulla nostra politica e principi sul{" "}
-      </span>
-      <span class="c6">
-        <a
-          class="c5"
-          href="https://www.google.com/url?q=https://voxcheck.voxukraine.org/&amp;sa=D&amp;source=editors&amp;ust=1681801293173724&amp;usg=AOvVaw2D_--a5qaK07hL9fo96T4l"
-        >
-          sito web
-        </a>
-      </span>
-      <span class="c1">&nbsp;ufficiale di VoxCheck. &nbsp;</span>
-    </p>
-    <p class="c3"><br>Nel maggio del 2022, VoxCheck ha presentato il primo <span class="c6"><a class="c5" href="https://www.google.com/url?q=https://rusdisinfo.voxukraine.org/&amp;sa=D&amp;source=editors&amp;ust=1681801293174207&amp;usg=AOvVaw0ZmF_M5SehREbz7toJJsHy">database</a></span><span class="c1">&nbsp;delle narrazioni di disinformazione russe in due media europei: italiano e tedesco. La prima versione di "Propaganda Diary" includeva i media proprio di questi paesi, perché poco prima dell'invasione russa su vasta scala dell'Ucraina, la Germania e l'Italia erano tra i principali obiettivi della propaganda del Cremlino in Europa. </p>
-    
-    
+    AboutIt
     </>
   );
 }

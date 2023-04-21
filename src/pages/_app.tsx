cc8eafa0429ad223e13de9f013ce5b3011a7c832
Serge <sergesoroka@gmail.com>
2023-04-21 17:55:33 +0300

    Added GoogleAnalitics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import Footer from '@/components/Footer/Footer';
 import Header from '@/components/Header/Header';
 import '@/styles/globals.css'
+import GoogleAnalitics from '../../utils/GoogleAnalitics'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Header />
+      <GoogleAnalitics />
       <Component {...pageProps} />
       <Footer />
     </>

d5a82301c1f9ec65f81cf3912496601d70e69a06
Serge <sergesoroka@gmail.com>
2023-04-25 13:00:07 +0300

    First Month Narrative

--- src/components/BarChart/BarChart.tsx
+++ src/components/BarChart/BarChart.tsx
@@ -24,62 +24,62 @@ const dataPlaceholder = [
 const BarChart = () => {
   const router = useRouter();
   const { month } = router.query;
-  // const data = [
-  //   {
-  //     name: "01",
-  //     score: commonStatistic("2022-01-01", "2022-01-31", "Fake").length * 10,
-  //   },
-  //   {
-  //     name: "02",
-  //     score: commonStatistic("2022-02-01", "2022-02-31", "Fake").length * 10,
-  //   },
-  //   {
-  //     name: "03",
-  //     score: commonStatistic("2022-03-01", "2022-03-31", "Fake").length * 10,
-  //   },
-  //   {
-  //     name: "04",
-  //     score: commonStatistic("2022-04-01", "2022-04-31", "Fake").length * 10,
-  //   },
-  //   {
-  //     name: "05",
-  //     score: commonStatistic("2022-05-01", "2022-05-31", "Fake").length * 10,
-  //   },
-  //   {
-  //     name: "06",
-  //     score: commonStatistic("2022-06-01", "2022-06-31", "Fake").length * 10,
-  //   },
-  //   {
-  //     name: "07",
-  //     score: commonStatistic("2022-07-01", "2022-07-31", "Fake").length * 10,
-  //   },
-  //   {
-  //     name: "08",
-  //     score: commonStatistic("2022-08-01", "2022-08-31", "Fake").length * 10,
-  //   },
-  //   {
-  //     name: "09",
-  //     score: commonStatistic("2022-09-01", "2022-09-31", "Fake").length * 10,
-  //   },
-  //   {
-  //     name: "10",
-  //     score: commonStatistic("2022-10-01", "2022-10-31", "Fake").length * 10,
-  //   },
-  //   {
-  //     name: "11",
-  //     score: commonStatistic("2022-11-01", "2022-11-31", "Fake").length * 10,
-  //   },
-  //   {
-  //     name: "12",
-  //     score: commonStatistic("2022-12-01", "2022-12-31", "Fake").length * 10,
-  //   },
-  // ];
+  const data = [
+    {
+      name: "01",
+      score: commonStatistic("2022-01-01", "2022-01-31", "Fake").length * 10,
+    },
+    {
+      name: "02",
+      score: commonStatistic("2022-02-01", "2022-02-31", "Fake").length * 10,
+    },
+    {
+      name: "03",
+      score: commonStatistic("2022-03-01", "2022-03-31", "Fake").length * 10,
+    },
+    {
+      name: "04",
+      score: commonStatistic("2022-04-01", "2022-04-31", "Fake").length * 10,
+    },
+    {
+      name: "05",
+      score: commonStatistic("2022-05-01", "2022-05-31", "Fake").length * 10,
+    },
+    {
+      name: "06",
+      score: commonStatistic("2022-06-01", "2022-06-31", "Fake").length * 10,
+    },
+    {
+      name: "07",
+      score: commonStatistic("2022-07-01", "2022-07-31", "Fake").length * 10,
+    },
+    {
+      name: "08",
+      score: commonStatistic("2022-08-01", "2022-08-31", "Fake").length * 10,
+    },
+    {
+      name: "09",
+      score: commonStatistic("2022-09-01", "2022-09-31", "Fake").length * 10,
+    },
+    {
+      name: "10",
+      score: commonStatistic("2022-10-01", "2022-10-31", "Fake").length * 10,
+    },
+    {
+      name: "11",
+      score: commonStatistic("2022-11-01", "2022-11-31", "Fake").length * 10,
+    },
+    {
+      name: "12",
+      score: commonStatistic("2022-12-01", "2022-12-31", "Fake").length * 10,
+    },
+  ];
 
   return (
     <div>
       <div className={styles.BarChart}>
         <svg className={styles.barChart} style={{ transform: "scaleY(-1)" }}>
-          {dataPlaceholder.map((item, i) => {
+          {data.map((item, i) => {
             let color = i % 2 === 0 ? "#CDCDCD" : "#e4e4e4";
             return (
               <Link key={item.name} href={`/month/${item.name}`}>
